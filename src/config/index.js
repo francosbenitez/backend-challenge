e49dcd617a1b795dcd799f98ca823a4e71b2a088
francobenitez <franco.benitez@hogarthww.com>
2022-05-26 19:33:08 -0300

    chore: change db config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "challenge-db",
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b9f45cf270e05b",
+  PASSWORD: "fd460bc2",
+  DB: "heroku_71b0a1bf689fa92",
   dialect: "mysql",
   authentication: {
     jwtSecret: process.env.JWT_SECRET || "secret",
